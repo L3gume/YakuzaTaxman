bf12154e69e0f42af63a68509531d6d3f221db4f
evanlaflamme <evanlaflamme@hotmail.com>
2018-03-23 22:10:59 -0400

    Initial work of game manager

---
+++ someProject/Assets/scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+	// Timer
+	public Text TimerText;
+	private float _timeLeft = 120.0f;
+	
+	// Score
+	public Text ScoreText;
+	private int _score = 0;
+	
+	// Post it
+	private List<Tuple<string, string>> _currentPostItValues;
+	
+	// Paper
+
+	// Use this for initialization
+	void Start ()
+	{
+		// Initialize text values
+		TimerText.text = "Time: " + Mathf.Round(_timeLeft);
+		ScoreText.text = "Score: " + _score;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// Update timer
+		_timeLeft -= Time.deltaTime;
+		TimerText.text = "Time: " + Mathf.Round(_timeLeft);
+
+		// Check if time has run out
+		if (_timeLeft <= 0.0f)
+		{
+			// Display game over screen with score
+		}
+		
+		// Check if paper is complete
+		if (isPaperComplete())
+		{
+			// Update score
+			_score++;
+
+			// Get new post it
+			
+			// Create new paper
+		}
+	}
+
+	// Checks whether the player has completed the needed input for the paper
+	bool isPaperComplete()
+	{
+		return true;
+	}
+}
