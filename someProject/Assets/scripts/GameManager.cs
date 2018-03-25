ba714e9960d711ed3f0cc1c1672a158f2b14d96b
L3gume <justin.tremblay@mail.mcgill.ca>
2018-03-24 21:45:15 -0400

    Fixed bug

--- someProject/Assets/scripts/GameManager.cs
+++ someProject/Assets/scripts/GameManager.cs
@@ -368,15 +368,16 @@ public class GameManager : MonoBehaviour
     //Shuffles the list
     private List<E> ShuffleList<E>(List<E> inputList)
     {
+        List<E> _inputList = new List<E>(inputList);
         List<E> randomList = new List<E>();
 
         System.Random r = new System.Random();
         int randomIndex = 0;
-        while (inputList.Count > 0)
+        while (_inputList.Count > 0)
         {
-            randomIndex = r.Next(0, inputList.Count); //Choose a random object in the list
-            randomList.Add(inputList[randomIndex]); //add it to the new, random list
-            inputList.RemoveAt(randomIndex); //remove to avoid duplicates
+            randomIndex = r.Next(0, _inputList.Count); //Choose a random object in the list
+            randomList.Add(_inputList[randomIndex]); //add it to the new, random list
+            _inputList.RemoveAt(randomIndex); //remove to avoid duplicates
         }
 
         return randomList; //return the new random list
